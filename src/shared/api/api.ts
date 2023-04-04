600781eb93f661bae9be09f94f9411a9d78f8419
Roman Adamchik <roman.adamchik@scandiweb.com>
2023-04-04 17:09:08 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,12 @@ import { LOCAL_STORAGE_AUTH_DATA_KEY } from 'shared/const/localStorage';
 export const $api = axios.create({
   baseURL: GLOBAL_API_URL,
   timeout: 1000,
-  headers: {
-    authorization: localStorage.getItem(LOCAL_STORAGE_AUTH_DATA_KEY) || '',
-  },
+});
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.Authorization = localStorage.getItem(LOCAL_STORAGE_AUTH_DATA_KEY) || '';
+  }
+
+  return config;
 });
